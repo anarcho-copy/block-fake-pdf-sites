bc7d545fa07ddca77501f7a983b2be55537c92f1
anarcho-copy <anarchocopy@protonmail.com>
2022-06-30 10:19:13 +0300

    update: lib/ublacklist2hosts.sh

--- lib/ublacklist2hosts.sh
+++ lib/ublacklist2hosts.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-sed 's/*:\/\///g; s/\/\*//g; s/www.//g' | sort -u
+# print usage info
+usage() {
+	echo "Usage: ${BASH_SOURCE[0]} <file>"
+        exit 1
+} ; if [[ "$#" -lt 1 ]]; then usage; fi
 
+# check options
+if [[ ! -f "${1}" ]]; then
+	echo "${1} does not exist"
+	exit 1
+fi
+
+# main
+sed 's/*:\/\///g; s/\/\*//g; s/www.//g' "${1}" | sort -u > "${1}.list"
+{ sleep 10; rm -f "${1}"; } &
